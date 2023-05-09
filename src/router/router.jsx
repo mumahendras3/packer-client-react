32f0ed5b5a3cdebfa656b2b9e5584a5faaa979c8
Ghandur <ghandurathallah10@gmail.com>
2023-05-09 17:28:19 +0700

    feat:swal-alert

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,50 +1,48 @@
-import { createBrowserRouter, redirect } from 'react-router-dom'
-import Home from '../pages/Home';
-import Login from '../pages/Login';
-import Register from '../pages/Register'
-import Watchlist from '../pages/Watchlist';
-import AddWatchlist from '../pages/AddWatchlist';
-import AddTask from '../pages/AddTask';
-import Layout from '../layout/Layout';
-import Tasklist from '../pages/Tasklist';
-import TaskDetail from '../pages/TaskDetail';
+import { createBrowserRouter, redirect } from "react-router-dom";
+import Swal from 'sweetalert2'
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Watchlist from "../pages/Watchlist";
+import AddWatchlist from "../pages/AddWatchlist";
+import AddTask from "../pages/AddTask";
+import Layout from "../layout/Layout";
+import Tasklist from "../pages/Tasklist";
+
+
 
 const router = createBrowserRouter([
-    {
-        element: <Layout />,
-        loader: () => {
-            if (localStorage.access_token) {
-                return null
-            }
-            return redirect('/login')
-        },
-        children: [
-            {
-                path: "/",
-                element: <Home />,
-            },
-            
-            {
-                path: "/watchlist",
-                element: <Watchlist />,
-            },
-            {
-                path: "/addwatchlist",
-                element: <AddWatchlist />,
-            },
-            {
-                path: "/addtask",
-                element: <AddTask />,
-            },
-            {
-                path: "/tasklist",
-                element: <Tasklist />,
-            },
-            {
-                path: "/taskdetail",
-                element: <TaskDetail />,
-            },
-        ],
+  {
+    element: <Layout />,
+    loader: () => {
+      if (localStorage.access_token) {
+        return null;
+      }
+      return redirect("/login");
+    },
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+
+      {
+        path: "/watchlist",
+        element: <Watchlist />,
+      },
+      {
+        path: "/addwatchlist",
+        element: <AddWatchlist />,
+      },
+      {
+        path: "/addtask",
+        element: <AddTask />,
+      },
+      {
+        path: "/tasklist",
+        element: <Tasklist />,
+      },
+    ],
   },
   {
     path: "/login",
